b93528310a65298cca4c94e646f203d0b2865315
srahlbaba <srahlbaba@gmail.com>
2015-07-23 16:47:36 -0400

    phase 1

---
+++ src/ReverseOnlyWords.java
@@ -0,0 +1,24 @@
+public class ReverseOnlyWords {
+
+	public static String reverse(String str) {
+
+		if (str == null || str.length() == 0) {
+			return "";
+		}
+
+		String[] stringArray = str.split(" ");
+		StringBuilder sb = new StringBuilder();
+		for (int i = stringArray.length - 1; i >= 0; --i) {
+			if (i != 0) {
+				sb.append(stringArray[i]).append(" ");
+			} else if (i == 0) {
+				sb.append(stringArray[i]);
+			}
+		}
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(ReverseOnlyWords.reverse("the sky is blue"));
+	}
+}
